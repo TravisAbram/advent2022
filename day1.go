eb56d1354096849d69e75f5b8a73260f9807e33a
Travis Abram <inthought@gmail.com>
2022-12-01 18:55:59 -0800

    feat: complete day 1 both parts

--- day1.go
+++ day1.go
@@ -24,20 +24,56 @@ func main() {
 		fileLines = append(fileLines, fileScanner.Text())
 	}
 
-	group_elves(fileLines)
+	totals := create_totals(fileLines)
+	max, location := find_max(totals)
+	fmt.Println(max, location)
+	fmt.Println(sum_max_values(totals, 3))
 
 }
 
-func group_elves(fileLines []string){
+func create_totals(fileLines []string)([]int){
+	var totals []int
+	total := 0
 	for _, line := range fileLines {
 		if len(line) == 0 {
-			fmt.Println("blank")
+			totals = append(totals, total)
+			total = 0
 		} else {
 			intLine, err := strconv.Atoi(line)
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Println(intLine)
+			total += intLine
 		}
 	}
+	// Collect the last total if the file doesn't end with a blank line
+	totals = append(totals, total)
+	return totals
+}
+
+func find_max(slice []int)(int, int){
+	max := 0
+	location := 0
+	for i, x := range slice {
+		if x > max {
+			max = x
+			location = i
+		}
+	}
+	return max, location
+}
+
+func remove_element(slice []int, index int)[]int{
+	return append(slice[:index], slice[index+1:]...)
+}
+
+func sum_max_values(slice []int, top int) int {
+	grand_total := 0
+	for i := 0; i < top; i++{
+		max, location := find_max(slice)
+		grand_total += max
+		fmt.Println(max)
+		slice = remove_element(slice, location)
+	}
+	return grand_total
 }
