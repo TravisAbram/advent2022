d1ad1319b94da7037c8832e4f4785bb03c3d065e
Travis Abram <inthought@gmail.com>
2022-12-01 22:24:27 -0800

    feat: add day 2 part 1

---
+++ day2/day2.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+)
+
+func main() {
+	readFile, err := os.Open("input")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	var fileLines []string
+
+	for fileScanner.Scan(){
+		fileLines = append(fileLines, fileScanner.Text())
+	}
+	for _, line := range fileLines {
+		fmt.Println(line)
+	}
+	
+	fmt.Println(calculateMatch(fileLines))
+}
+
+func calculateMatch(rounds []string) int {
+	var total int
+	for _, round := range rounds {
+		total += calculateScore(round)
+	}
+	return total
+}
+
+func calculateScore(move string) int {
+	var theirs, mine string = string(move[0]), string(move[2])
+	result := winLoseDraw(theirs, mine)
+	shape := shapeScore(mine)
+	return result + shape
+}
+
+func winLoseDraw(theirs, mine string) int {
+	switch round := theirs + mine; round {
+	case "AX", "BY", "CZ": // all draws
+		return 3
+	case "AY", "BZ", "CX": // all wins
+		return 6
+	default:
+		return 0
+	}
+}
+
+func shapeScore(mine string) int {
+	switch mine {
+	case "X":
+		return 1
+	case "Y":
+		return 2
+	default:
+		return 3
+	}
+}
