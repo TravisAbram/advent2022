20a1532289c845c5313ae27175748a6a76848dcb
Travis Abram <inthought@gmail.com>
2022-12-01 22:44:20 -0800

    feat: add part 2

--- day2/day2.go
+++ day2/day2.go
@@ -22,6 +22,7 @@ func main() {
 		fileLines = append(fileLines, fileScanner.Text())
 	}
 	fmt.Println("Part 1: ", calculateMatch(fileLines))
+	fmt.Println("Part 2: ", calculateActualMatch(fileLines))
 }
 
 func calculateMatch(rounds []string) int {
@@ -32,8 +33,25 @@ func calculateMatch(rounds []string) int {
 	return total
 }
 
+func calculateActualMatch(rounds []string) int {
+	var total int
+	for _, round := range rounds {
+		total += calculateActualScore(round)
+	}
+	return total
+}
+
+
 func calculateScore(move string) int {
 	var theirs, mine string = string(move[0]), string(move[2])
+	result := winLoseDraw(theirs, mine)
+	shape := shapeScore(mine)
+	return result + shape
+}
+
+func calculateActualScore(move string) int {
+	var theirs string = string(move[0])
+	mine := getMyMove(move)
 	result := winLoseDraw(theirs, mine)
 	shape := shapeScore(mine)
 	return result + shape
@@ -58,5 +76,17 @@ func shapeScore(mine string) int {
 		return 2
 	default:
 		return 3
+	}
+}
+
+func getMyMove(move string) string {
+	var theirs, result string = string(move[0]), string(move[2])
+	switch strategy := theirs + result; strategy{
+	case "AX", "BZ", "CY":
+		return "Z" // sissors
+	case "AY", "BX", "CZ":
+		return "X" // rock
+	default:
+		return "Y"
 	}
 }
