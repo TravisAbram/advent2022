e658b01b651541685ea63ec51158b8273880a16d
Travis Abram <tabram@starbucks.com>
2022-12-03 22:32:02 -0800

    feat: finish part 1

--- day3/day3.go
+++ day3/day3.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"bytes"
+	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -22,23 +22,31 @@ func main() {
 	for fileScanner.Scan() {
 		fileLines = append(fileLines, fileScanner.Text())
 	}
+	
+	fmt.Println("Part 1:", sumPriorities(fileLines))
 
-	first, second := splitCompartments("abcdeb")
-	match := findMatch(first, second)
-	fmt.Println(match)
-	fmt.Println(getCharacterScore(match))
 }
 
-func splitCompartments(rucksack string)(first, second string){
+func sumPriorities(sacks []string) int {
+	var total int
+	for _, rucksack := range sacks {
+		first, second := splitCompartments(rucksack)
+		match := findMatch(first, second)
+		total += getCharacterScore(match)
+	}
+	return total
+}
+
+func splitCompartments(rucksack string) (first, second string) {
 	first, second = rucksack[:len(rucksack)/2], rucksack[(len(rucksack)/2):]
 	return
 }
 
-func findMatch(first, second string) rune {
+func findMatch(first, second string) string {
 	for _, x := range first {
 		for _, y := range second {
 			if x == y {
-				return strconv.(x)
+				return string(x)
 			}
 		}
 	}
@@ -46,6 +54,6 @@ func findMatch(first, second string) rune {
 }
 
 func getCharacterScore(character string) int {
-	score, _ := strconv.Atoi(character)
-	return score
+	priority := []byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+	return bytes.IndexAny(priority, character) + 1
 }
